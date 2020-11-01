11c509bc317b588a3aa2978ca82afb10751943f1
MahmoudSiliman <modysiliman75@gmail.com>
2020-11-01 15:05:27 +0200

    first commit

---
+++ U-Care Admin DashBoard/edit-profile.php
@@ -0,0 +1,81 @@
+<?php include("layouts/header.php") ?>
+
+	<div class="intro-y flex items-center mt-8">
+		<h2 class="text-lg font-medium mr-auto">
+			Edit Profile
+		</h2>
+	</div>
+
+	<!-- BEGIN: Profile Info -->
+	<div class="grid grid-cols-12 gap-6 mt-5">
+
+
+		<div class="intro-y box px-5 pt-5 mt-5 col-span-12 sm:col-span-12 lg:col-span-4">
+			<div class="flex flex-col lg:flex-row pb-5 -mx-5">
+
+				<div class="flex flex-1 flex-wrap justify-center px-5 items-center justify-center">
+
+					<div class="w-20 h-20 sm:w-24 sm:h-24 flex-none lg:w-32 lg:h-32 image-fit relative">
+						<img alt="User Img" class="rounded-full image-preview" src="dist/images/profile-7.jpg">
+					</div>
+
+					<div class="w-full ml-5">
+						<div class="w-full flex justify-center truncate sm:whitespace-normal font-medium text-lg mt-3">User 1</div>
+						<div class="text-gray-600 flex justify-center">Head Doctor</div>
+					</div>
+
+					<input class="mt-8 mr-3 ml-3 image" type="file" > 
+				</div>
+
+			</div>
+		</div>
+
+
+		<div class="intro-y box px-5 pt-5 mt-5 col-span-12 sm:col-span-12 lg:col-span-8">
+
+			<form>
+				<div class="grid grid-cols-12 gap-6">
+					<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+						<input type="text" class="input w-full border mt-2" placeholder="Name"> 
+					</div>
+
+					<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+						<input type="text" class="input w-full border mt-2" placeholder="Phone Number"> 
+					</div>
+
+					<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+						<select data-search="true" class="tail-select w-full">
+							<option value="1">Select City</option>
+							<option value="2">Cairo</option>
+							<option value="3">Dakahlya</option>
+							<option value="4">Cairo</option>
+						</select> 
+					</div>
+
+					<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+						<select data-search="true" class="tail-select w-full">
+							<option value="1">Select Area</option>
+							<option value="2">Doky</option>
+							<option value="3">6 Oct</option>
+						</select> 
+					</div>
+
+					<div class="flex flex-col col-span-12 sm:col-span-12 lg:col-span-6 lg:flex-row p-5 -mx-5">
+						<div class="editor" name="editor"> 
+						</div> 
+					</div>
+				</div>
+			</form>
+
+			</div>
+
+		</div>
+
+	</div>
+
+
+
+
+
+
+<?php include("layouts/footer.php") ?>
---
+++ U-Care Admin DashBoard/layouts/footer.php
@@ -0,0 +1,29 @@
+</div>
+            <!-- END: Content -->
+            
+        </div>
+
+        <!-- BEGIN: JS Assets-->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places"></script>
+        <script src="dist/js/app.js"></script>
+
+        <script>
+        $(document).ready(function () {
+            $(".image").on('change',function() {
+                if (this.files && this.files[0]) {
+                    var reader = new FileReader();
+
+                    reader.onload = function(e) {
+                        $('.image-preview').attr('src', e.target.result);
+                    }
+
+                    reader.readAsDataURL(this.files[0]); // convert to base64 string
+                }
+            });
+        });
+        </script>
+        <!-- END: JS Assets-->
+    </body>
+</html>
